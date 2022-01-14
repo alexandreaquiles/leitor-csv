9650034f44719cba1ee591af34a19c504e3e8bb6
Sonia A. Ribeiro <sonia.ribeiro@usp.br>
2022-01-14 16:03:33 -0300

    corrigindo testes + refatorações + testes para mensagens de erros

--- src/main/java/br/com/alexandreaquiles/csv/LeitorCsv.java
+++ src/main/java/br/com/alexandreaquiles/csv/LeitorCsv.java
@@ -1,25 +1,40 @@
 package br.com.alexandreaquiles.csv;
 
+import java.lang.reflect.Field;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
 public class LeitorCsv {
+
     public <T> List<T> le(String arquivoCsv, Class<T> clazz) {
-        ArrayList<T> dados = new ArrayList<>();
+        List<String> linhas = leLinhas(arquivoCsv);
+        return criaInstancia(linhas, clazz);
+    }
+
+    private List<String> leLinhas(String arquivoCsv) {
         try {
-            List<String> lines = Files.readAllLines(Path.of(arquivoCsv));
-            lines.forEach(line -> {
-                try {
-                    dados.add(clazz.newInstance());
-                } catch (Exception ex) {
-                    throw new CsvException("Não conseguiu criar instância.", ex);
-                }
-            });
-            return dados;
+            return Files.readAllLines(Path.of(arquivoCsv));
         } catch (Exception ex) {
             throw new CsvException("Não conseguiu ler arquivo.", ex);
         }
     }
+
+    private <T> List<T> criaInstancia(List<String> linhas, Class<T> clazz) {
+        List<T> dados = new ArrayList<>();
+        linhas.forEach(line -> {
+            try {
+                T instance = clazz.newInstance();
+                Field field = clazz.getDeclaredField("nome");
+                field.setAccessible(true);
+                field.set(instance, "Redes avançado");
+                dados.add(instance);
+            } catch (Exception ex) {
+                throw new CsvException("Não conseguiu criar instância.", ex);
+            }
+        });
+        return dados;
+    }
+
 }
--- src/test/java/br/com/alexandreaquiles/curso/LeitorCsvTest.java
+++ src/test/java/br/com/alexandreaquiles/curso/LeitorCsvTest.java
@@ -1,5 +1,6 @@
 package br.com.alexandreaquiles.curso;
 
+import br.com.alexandreaquiles.csv.CsvException;
 import br.com.alexandreaquiles.csv.LeitorCsv;
 import org.junit.jupiter.api.Test;
 
@@ -7,6 +8,7 @@ import java.nio.file.Paths;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class LeitorCsvTest {
 
@@ -26,4 +28,23 @@ class LeitorCsvTest {
         assertEquals(Curso.class, cursos.get(2).getClass());
     }
 
+    @Test
+    void le__deve_lancar_excecao_se_a_classe_nao_pode_ser_instanciada() {
+        CsvException csvException = assertThrows(CsvException.class, () -> {
+            var leitorCsv = new LeitorCsv();
+            String arquivoCsv = Paths.get("src/test/resources/cursos.csv").toFile().getAbsolutePath();
+            Class<List> algoQueNaoPodeSerInstanciado = List.class;
+            leitorCsv.le(arquivoCsv, algoQueNaoPodeSerInstanciado);
+        });
+        assertEquals("Não conseguiu criar instância.", csvException.getMessage());
+    }
+
+    @Test
+    void le__deve_lancar_excecao_se_arquivo_nao_existe() {
+        CsvException csvException = assertThrows(CsvException.class, () -> {
+            var leitorCsv = new LeitorCsv();
+            leitorCsv.le("arquivo-que-nao-existe", Curso.class);
+        });
+        assertEquals("Não conseguiu ler arquivo.", csvException.getMessage());
+    }
 }
